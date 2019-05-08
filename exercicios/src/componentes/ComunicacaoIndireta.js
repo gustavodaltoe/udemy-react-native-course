28ec61c1d35c2833c303f4b48c178b0ad2f97891
Gustavo Daltoé <gustavo.daltoe@act-dev.net>
2019-05-08 11:29:46 -0300

    Comunicacao Indireta

---
+++ exercicios/src/componentes/ComunicacaoIndireta.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import { View, Text, TextInput } from 'react-native'
+import Padrao from '../estilo/Padrao'
+
+export const Entrada = props =>
+    <View>
+        <TextInput value={props.texto}
+            style={Padrao.input}
+            onChangeText={props.chamarQuandoMudar} />
+    </View>
+
+export default class TextoSincronizado extends Component {
+
+    state = {
+        texto: ''
+    }
+
+    alterarText = texto => {
+        //this.setState({ texto: texto })
+        this.setState({ texto }) // Forma reduzida devido ao nome do param ser igual ao obj
+    }
+
+    render() {
+        return (
+            <View>
+                <Text style={Padrao.font40}>{this.state.texto}</Text>
+                <Entrada texto={this.state.texto}
+                    chamarQuandoMudar={this.alterarTexto} />
+            </View>
+        )
+    }
+}
