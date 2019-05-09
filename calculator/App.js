face3743f185a093b89b3b62049b9ea51edbb8bc
Gustavo Daltoé <gustavo.daltoe@act-dev.net>
2019-05-09 11:04:41 -0300

    calculadora completa

--- calculator/App.js
+++ calculator/App.js
@@ -3,22 +3,63 @@ import { Platform, StyleSheet, Text, View } from 'react-native'
 import Button from './src/components/Button'
 import Display from './src/components/Display'
 
-export default class App extends Component {
+const initialState = {
+  displayValue: '0',
+  clearDisplay: false,
+  operation: null,
+  values: [0, 0],
+  current: 0, // Diz qual o index do value estamos setando
+}
 
-  state = {
-    displayValue: '0'
-  }
+export default class App extends Component {
+  state = { ...initialState }
 
   addDigit = n => {
-    this.setState({ displayValue: n })
+    const clearDisplay = this.state.displayValue === '0'
+      || this.state.clearDisplay
+
+    if (n === '.' && !clearDisplay && this.state.displayValue.includes('.')) return
+
+    const currentValue = clearDisplay ? '' : this.state.displayValue
+    const displayValue = currentValue + n
+    this.setState({ displayValue, clearDisplay: false })
+
+    if (n !== '.') {
+      const newValue = parseFloat(displayValue)
+      const values = [...this.state.values]
+      values[this.state.current] = newValue
+      this.setState({ values })
+    }
   }
 
   clearMemory = () => {
-    this.setState({ displayValue: '0' })
+    this.setState({ ...initialState })
   }
 
   setOperation = operation => {
-    
+    if (this.state.current === 0) {
+      this.setState({ operation, current: 1, clearDisplay: true })
+    } else {
+      const equals = operation === '='
+      const values = [...this.state.values]
+      try {
+        /* eval avalia expressões, ou seja,
+           passando v[0] = 2, operation = '+' e v[1] = 3
+           a função traduz para '2 + 3' e retorna o resultado = 5 */
+        values[0] = eval(`${values[0]} ${this.state.operation} ${values[1]}`)
+      } catch (e) {
+        values[0] = this.state.values[0]
+      }
+
+      values[1] = 0
+      this.setState({
+        displayValue: `${values[0]}`, //Interpolar para sempre ser string
+        operation: equals ? null : operation,
+        current: equals ? 0 : 1,
+        clearDisplay: !equals,
+        values,
+      })
+    }
   }
 
   render() {
@@ -27,22 +68,22 @@ export default class App extends Component {
         <Display value={this.state.displayValue} />
         <View style={styles.buttons}>
           <Button label='AC' triple onClick={this.clearMemory} />
-          <Button label='/' operation onClick={() => this.setOperation('/')} />
-          <Button label='7' onClick={() => this.addDigit('7')} />
-          <Button label='8' onClick={() => this.addDigit('8')} />
-          <Button label='9' onClick={() => this.addDigit('9')} />
-          <Button label='*' operation onClick={() => this.setOperation('*')} />
-          <Button label='4' onClick={() => this.addDigit('4')} />
-          <Button label='5' onClick={() => this.addDigit('5')} />
-          <Button label='6' onClick={() => this.addDigit('6')} />
-          <Button label='-' operation onClick={() => this.setOperation('-')} />
-          <Button label='1' onClick={() => this.addDigit('1')} />
-          <Button label='2' onClick={() => this.addDigit('2')} />
-          <Button label='3' onClick={() => this.addDigit('3')} />
-          <Button label='+' operation onClick={() => this.setOperation('+')} />
-          <Button label='0' double onClick={() => this.addDigit('0')} />
-          <Button label='.' onClick={() => this.addDigit('.')} />
-          <Button label='=' operation onClick={() => this.setOperation('=')} />
+          <Button label='/' operation onClick={this.setOperation} />
+          <Button label='7' onClick={this.addDigit} />
+          <Button label='8' onClick={this.addDigit} />
+          <Button label='9' onClick={this.addDigit} />
+          <Button label='*' operation onClick={this.setOperation} />
+          <Button label='4' onClick={this.addDigit} />
+          <Button label='5' onClick={this.addDigit} />
+          <Button label='6' onClick={this.addDigit} />
+          <Button label='-' operation onClick={this.setOperation} />
+          <Button label='1' onClick={this.addDigit} />
+          <Button label='2' onClick={this.addDigit} />
+          <Button label='3' onClick={this.addDigit} />
+          <Button label='+' operation onClick={this.setOperation} />
+          <Button label='0' double onClick={this.addDigit} />
+          <Button label='.' onClick={this.addDigit} />
+          <Button label='=' operation onClick={this.setOperation} />
         </View>
       </View>
     )
